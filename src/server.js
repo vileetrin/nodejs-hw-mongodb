02f3ecd893989e2dd628889827ec0e8673fd0683
Violetta Velykotrav <violettavelykotrav@gmail.com>
2024-08-07 01:46:39 +0300

    add task

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import contactRoutes from './routers/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,45 +25,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
+  app.use(contactRoutes);
 
-app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id {contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(notFoundHandler);
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      status: 404,
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      status: 500,
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,24 @@ export const getContactById = async (contactId) => {
   const contact = await Contacts.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await Contacts.create(payload);
+  return contact;
+}
+
+export const updateContact = async (contactId, payload, option = {}) => {
+  const result = await Contacts.findByIdAndUpdate({ _id: contactId }, payload, { new: true, includeResultMetadata: true, ...option, });
+
+  if (!result || !result.value) return null;
+
+  return {
+    contact: result.value,
+    isNew: Boolean(result?.lastErrorObject?.upserted),
+  };
+}
+
+export const deleteContact = async (contactId) => {
+  const deletedContact = await Contacts.findByIdAndDelete(contactId);
+  return deletedContact;
+}
